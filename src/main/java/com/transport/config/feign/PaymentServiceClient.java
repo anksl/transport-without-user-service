66c0ff057de76d3d8dc0e825ebcf777dd3eb8bc2
Anna Kisel <annakisel524013@gmail.com>
2023-07-25 17:36:04 +0300

    changed annotation

--- src/main/java/com/transport/config/feign/PaymentServiceClient.java
+++ src/main/java/com/transport/config/feign/PaymentServiceClient.java
@@ -3,6 +3,7 @@ package com.transport.config.feign;
 import com.transport.api.dto.PaymentDto;
 import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 @FeignClient(name = "payment-service")
 public interface PaymentServiceClient {
 
-    @PostMapping("/api/payments")
-    @Headers("Content-Type: application/json")
+    @PostMapping(value = "/api/payments", produces = MediaType.APPLICATION_JSON_VALUE)
     void savePayment(PaymentDto paymentDto);
 
-    @GetMapping("/api/payments/{id}")
-    @Headers("Content-Type: application/json")
+    @GetMapping(value = "/api/payments/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     PaymentDto getPaymentById(@PathVariable(value = "id") Long id);
 }
--- src/main/java/com/transport/config/feign/UserServiceClient.java
+++ src/main/java/com/transport/config/feign/UserServiceClient.java
@@ -3,12 +3,12 @@ package com.transport.config.feign;
 import com.transport.api.dto.UserDto;
 import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "user-service")
 public interface UserServiceClient {
 
-    @GetMapping("/api/users/current")
-    @Headers("Content-Type: application/json")
+    @GetMapping(value = "/api/users/current", produces = MediaType.APPLICATION_JSON_VALUE)
     UserDto getCurrentUser();
 }
